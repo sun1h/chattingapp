500a3967eaefba19e6dd43aa6833f6aff5e7e128
최혜원 <hwchoi33@naver.com>
2023-06-28 20:05:53 +0900

    firebase+vue

---
+++ src/db.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlSuP4CM0mBDCqiAd8nGwRyA_w9703z2A",
+  authDomain: "chatting-23edc.firebaseapp.com",
+  projectId: "chatting-23edc",
+  storageBucket: "chatting-23edc.appspot.com",
+  messagingSenderId: "603544811184",
+  appId: "1:603544811184:web:1d8425aa4227f08cd02d75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
